beeb05cee00bd719cce241745075df14b4445338
sk.karem <sk.karem@gmail.com>
2023-05-22 00:30:29 -0400

    build2

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ module.exports = {
         path: '',
       },
       // Ajusta la ruta base del proyecto para que funcione en GitHub Pages
-    //basePath: '/next-js-poke-memory',
-    //assetPrefix: '/next-js-poke-memory/', // Reemplaza esto con el nombre de tu repositorio
+    basePath: '/next-js-poke-memory',
+    assetPrefix: '/next-js-poke-memory', // Reemplaza esto con el nombre de tu repositorio
 
 
   };
