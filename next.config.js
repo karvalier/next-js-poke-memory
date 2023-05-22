35cc738538df561635c6ef5aa4609c1cfb7832c2
sk.karem <sk.karem@gmail.com>
2023-05-22 00:47:06 -0400

    fix img

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       },
       // Ajusta la ruta base del proyecto para que funcione en GitHub Pages
     basePath: '/next-js-poke-memory',
-    assetPrefix: '/next-js-poke-memory', // Reemplaza esto con el nombre de tu repositorio
+    assetPrefix: '/', // Reemplaza esto con el nombre de tu repositorio
 
 
   };
